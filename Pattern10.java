a5c4f4edd163c38502be5f1cc15410e4e85d69d3
iammanjotkaur <44539592+iammanjotkaur@users.noreply.github.com>
2018-10-31 00:33:54 +0530

    Add files via upload

---
+++ Pattern10.java
@@ -0,0 +1,22 @@
+﻿import java.lang.Math;
+public class Pattern10 {
+    public static void main(String args[]) {
+        
+       int row=5,n=1,k=row-1;
+       for(int i=1;i<=row;i++)
+       {   
+           n=i;k=row-1;
+           for(int j=1;j<=i;j++)
+           {
+               System.out.print(n+" ");
+               n=n+k;
+               k--;
+           }
+           
+           System.out.print("\n");
+       
+       
+           
+       }
+    }
+}
