6803098e8c347ac1d08945c82d6c3266f633f18b
iammanjotkaur <44539592+iammanjotkaur@users.noreply.github.com>
2018-10-31 00:53:20 +0530

    Add files via upload

---
+++ Pattern1.java
@@ -0,0 +1,16 @@
+﻿public class Pattern1 {
+    public static void main(String args[]) {
+        
+       int n=7,k=0;
+       for(int i=0;i<7;i++)
+       {     k=n;
+           for(int j=0;j<=i;j++)
+           {
+           
+               System.out.print(k);
+               k--;
+           }
+           System.out.print("\n");
+       }
+    }
+}
