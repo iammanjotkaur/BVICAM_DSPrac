106c765161768e29894b0f44a80c6c41191669f5
iammanjotkaur <44539592+iammanjotkaur@users.noreply.github.com>
2018-10-31 21:43:26 +0530

    Update Pattern7.java

--- pattern/Pattern7.java
+++ pattern/Pattern7.java
@@ -1,10 +1,17 @@
-﻿public class Pattern7 {
+/*
+11111 
+1   1 
+1   1 
+1   1 
+11111
+*/
+public class Pattern7 {
     public static void main(String args[]) {
         
        int n=4,k=1;
-       for(int i=0;i<n;i++)
+       for(int i=0;i<n;i++)//O(n^2)
        {     
-           for(int j=0;j<n;j++)
+           for(int j=0;j<n;j++)//O(n)
            {
                if((i==0)||(j==0)||(i==n-1)||(j==n-1))
                 System.out.print("1");
