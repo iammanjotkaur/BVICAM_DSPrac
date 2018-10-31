edb4ecea38570aef3cdbdc3daa97a00e98470050
iammanjotkaur <44539592+iammanjotkaur@users.noreply.github.com>
2018-10-31 21:41:53 +0530

    Update Pattern5.java

--- pattern/Pattern5.java
+++ pattern/Pattern5.java
@@ -1,11 +1,19 @@
-﻿public class Pattern5 {
+/*
+*
+***
+***** 
+******* 
+*********
+
+*/
+public class Pattern5 {
     public static void main(String args[]) {
         
        int n=7,k=1;
-       for(int i=0;i<7;i++)
+       for(int i=0;i<7;i++)//O(n^2)
        {     
            
-           for(int j=0;j<k;j++)
+           for(int j=0;j<k;j++)//O(n)
            {
            
                System.out.print("*");
