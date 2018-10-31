187d709133d14a029c48106519668ca4ddd8c4b5
iammanjotkaur <44539592+iammanjotkaur@users.noreply.github.com>
2018-10-31 21:46:18 +0530

    Update Pattern10.java

--- pattern/Pattern10.java
+++ pattern/Pattern10.java
@@ -1,12 +1,19 @@
-﻿import java.lang.Math;
+/*
+1
+26
+3 7 10
+4 8 11 13
+5 9 12 14 15
+*/
+import java.lang.Math;
 public class Pattern10 {
     public static void main(String args[]) {
         
        int row=5,n=1,k=row-1;
-       for(int i=1;i<=row;i++)
+       for(int i=1;i<=row;i++)//O(n^2)
        {   
            n=i;k=row-1;
-           for(int j=1;j<=i;j++)
+           for(int j=1;j<=i;j++)//O(n)
            {
                System.out.print(n+" ");
                n=n+k;
