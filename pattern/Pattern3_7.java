6083d2df1d83f62ca747f7727c9aff9fe83c18b3
manjot kaur <manjot.kaur97@gmail.com>
2018-11-11 14:57:11 +0530

    pattern3

--- pattern/Pattern3_7.java
+++ pattern/Pattern3_7.java
@@ -1,5 +1,13 @@
 /*
-
+Enter number of rows or col:
+4
+1     1 
+ 2   2 
+  3 3 
+   4 
+  3  3 
+ 2    2 
+1      1 
 
 
 */
@@ -12,10 +20,55 @@ class Pattern3_7
         Scanner sc=new Scanner(System.in);
         System.out.println("\nEnter number of rows or col:");
         n=sc.nextInt();
-        int space=0;
-        for(int i=0;i<5;i++)
+        int space=n,k=n-1,num=1,col;
+        for(int i=0;i<n;i++)
         {
-            
+            col=0;
+            while(k>=space)
+            {
+                System.out.print(" ");
+                k--;
+                col++;
+            }
+            System.out.print(num+" ");
+           col=col+1;
+            for(int j=col;j<=n-i;j++)
+            {
+                  System.out.print(" ");
+            }
+            if(num!=n)
+                   System.out.print(num+" ");
+            num++;
+            k=n-1;
+            space--;
+            col=0;
+            System.out.print("\n");
+        }
+        num-=2;
+        k=0;
+        space=n-2;
+         for(int i=0;i<n-1;i++)
+        {
+            col=0;
+             while(k<space)
+            {
+                System.out.print(" ");
+                k++;
+                col++;
+            }
+            System.out.print(num+" ");
+           col=col+1;
+            for(int j=col;j<n+i;j++)
+            {
+                  System.out.print(" ");
+            }
+            if(num!=4)
+                   System.out.print(num+" ");
+            num--;
+            k=0;
+            space--;
+            col=0;
+            System.out.print("\n");
         }
     }
 }
