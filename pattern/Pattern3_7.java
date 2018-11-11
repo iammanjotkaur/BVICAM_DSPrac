f71ddf4fe4791419b05a8ed1cc0f7a7d11ae22ca
manjot kaur <manjot.kaur97@gmail.com>
2018-11-11 14:37:53 +0530

    pattern3

---
+++ pattern/Pattern3_7.java
@@ -0,0 +1,21 @@
+/*
+
+
+
+*/
+import java.util.Scanner;
+class Pattern3_7
+{
+    public static void main(String[]args)
+    {
+        int n;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("\nEnter number of rows or col:");
+        n=sc.nextInt();
+        int space=0;
+        for(int i=0;i<5;i++)
+        {
+            
+        }
+    }
+}
